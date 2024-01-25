54b4370c6a842fc2c092d593fb67e5d41462dd62
Artur <fhnehbcvfubkjd@yandex.ru>
2024-01-25 15:45:26 +0500

    17-7

---
+++ 17-7.py
@@ -0,0 +1,15 @@
+def transpose(matr):
+    res=[]
+    n=len(matr)
+    m=len(matr[0])
+    for j in range(m):
+        tmp=[]
+        for i in range(n):
+            tmp=tmp+[matr[i][j]]
+        res=res+[tmp]
+    return res
+
+matrix = [[1]]
+transpose(matrix)
+for line in matrix:
+    print(*line)
